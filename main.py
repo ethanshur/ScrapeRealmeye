3d9bd37f1c7cd4f31afd9f75c5855ec2941580dc
Luke <lbrandes3@gmail.com>
2020-10-20 23:25:41 -0700

    refactor and clean up code

--- main.py
+++ main.py
@@ -1,211 +1,227 @@
 import bs4 as bs
 from urllib.request import Request, urlopen
 
-def parseWeapon(URL):
-    weaponParseReq = Request(URL, headers = {"User-Agent": "Mozilla/5.0"})
-    weaponPage = urlopen(weaponParseReq).read()
-    weaponSoup = bs.BeautifulSoup(weaponPage, "lxml")
 
-    tempString = "https://www.realmeye.com/wiki/"
-    name = URL
+def parse_weapon(url):
+    weapon_parse_req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+    weapon_page = urlopen(weapon_parse_req).read()
+    weapon_soup = bs.BeautifulSoup(weapon_page, "lxml")
 
-    name = name.replace(tempString, "")
+    temp_string = "https://www.realmeye.com/wiki/"
+    name = url
+
+    name = name.replace(temp_string, "")
     print(name)
-    img = weaponSoup.findAll("img")
-    imgURL = ""
+    img = weapon_soup.findAll("img")
+    img_url = ""
     for j in range(len(img)):
-        if img[j].get("alt").replace(" ", "-").lower() == name:
-            imgURL = "https:" + img[j].get("src")
+        if img[j].get("alt").replace(" ", "-").lower() is name:
+            img_url = "https:" + img[j].get("src")
             break
-    weaponDict = {"Name": name.replace("-", " "),"Img": imgURL, "Tier": -1, "Shots": -1, "Damage": -1, "Rate of Fire": 1.0}
-    weapon = weaponSoup.getText().split("\n")
+    weapon_dict = {"Name": name.replace("-", " "), "Img": img_url,
+                   "Tier": -1, "Shots": -1, "Damage": -1, "Rate of Fire": 1.0}
+    weapon = weapon_soup.getText().split("\n")
     for i in range(len(weapon) - 1):
-        if weapon[i] == "Tier":
+        if weapon[i] is "Tier":
             if (weapon[i+1]).isdigit():
-                weaponDict["Tier"] = int (weapon[i+1])
+                weapon_dict["Tier"] = int(weapon[i+1])
             elif "UT" in weapon[i+1]:
-                weaponDict["Tier"] = "UT"
+                weapon_dict["Tier"] = "UT"
             elif "ST" in weapon[i+1]:
-                weaponDict["Tier"] = "ST"
+                weapon_dict["Tier"] = "ST"
             else:
                 raise ValueError
-        elif weapon[i] == "Shots":
+        elif weapon[i] is "Shots":
             if " " in weapon[i+1]:
-                shotsParse = weapon[i + 1].split(" ")
+                shots_parse = weapon[i + 1].split(" ")
             else:
-                shotsParse = weapon[i + 1].split(" ")
-            if (shotsParse[0].isdigit()):
-                weaponDict["Shots"] = int(shotsParse[0])
-            else:
-                raise ValueError
-        elif weapon[i] == "Damage":
-            lowHighPull = weapon[i+1].split(" ")
-            lowHighPull = lowHighPull[0].split("–")
-            lowHigh = [lowHighPull[0], lowHighPull[1]]
-            if (lowHigh[0].isdigit() and lowHigh[1].isdigit()):
-                weaponDict["Damage"] = lowHigh
+                shots_parse = weapon[i + 1].split(" ")
+            if shots_parse[0].isdigit():
+                weapon_dict["Shots"] = int(shots_parse[0])
             else:
                 raise ValueError
-        elif weapon[i] == "Rate of Fire":
-            if weapon[i+1][-1] == "%":
-                rofPull = weapon[i+1][:-1]
-                rofPull = float (rofPull)/100
-                weaponDict["Rate of Fire"] = rofPull
+        elif weapon[i] is "Damage":
+            low_high_pull = weapon[i+1].split(" ")
+            low_high_pull = low_high_pull[0].split("–")
+            low_high = [low_high_pull[0], low_high_pull[1]]
+            if low_high[0].isdigit() and low_high[1].isdigit():
+                weapon_dict["Damage"] = low_high
             else:
                 raise ValueError
-        elif weapon[i] == "On Equip":
-            statsDict = {"HP": 0, "MP": 0, "ATT": 0, "DEF": 0, "DEX": 0, "SPD": 0, "WIS": 0, "VIT": 0}
+        elif weapon[i] is "Rate of Fire":
+            if weapon[i+1][-1] is "%":
+                rof_pull = weapon[i+1][:-1]
+                rof_pull = float(rof_pull)/100
+                weapon_dict["Rate of Fire"] = rof_pull
+            else:
+                raise ValueError
+        elif weapon[i] is "On Equip":
+            stats_dict = {"HP": 0, "MP": 0, "ATT": 0, "DEF": 0, "DEX": 0, "SPD": 0, "WIS": 0, "VIT": 0}
             stats = weapon[i + 1].split(",")
             for j in range(len(stats)):
-                if "HP" in stats[j]:
-                    statsDict["HP"] = float(stats[j].replace(" HP", ""))
-                elif "MP" in stats[j]:
-                    statsDict["MP"] = float(stats[j].replace(" MP", ""))
-                elif "ATT" in stats[j]:
-                    statsDict["ATT"] = float(stats[j].replace(" ATT", ""))
-                elif "DEF" in stats[j]:
-                    statsDict["DEF"] = float(stats[j].replace(" DEF", ""))
-                elif "DEX" in stats[j]:
-                    statsDict["DEX"] = float(stats[j].replace(" DEX", ""))
-                elif "SPD" in stats[j]:
-                    statsDict["SPD"] = float(stats[j].replace(" SPD", ""))
-                elif "WIS" in stats[j]:
-                    statsDict["WIS"] = float(stats[j].replace(" WIS", ""))
-                elif "VIT" in stats[j]:
-                    statsDict["VIT"] = float(stats[j].replace(" VIT", ""))
-            weaponDict["Stats"] = statsDict
-    print(weaponDict)
-    return weaponDict
+                arr = stats[j].split
+                stats_dict[arr[1]] = float(arr[0])
 
-def parseWeapons():
-    URLarray = ["daggers", "bows", "staves", "wands", "swords", "katanas"]
+                # if "HP" in stats[j]:
+                #     statsDict["HP"] = float(stats[j].replace(" HP", ""))
+                # elif "MP" in stats[j]:
+                #     statsDict["MP"] = float(stats[j].replace(" MP", ""))
+                # elif "ATT" in stats[j]:
+                #     statsDict["ATT"] = float(stats[j].replace(" ATT", ""))
+                # elif "DEF" in stats[j]:
+                #     statsDict["DEF"] = float(stats[j].replace(" DEF", ""))
+                # elif "DEX" in stats[j]:
+                #     statsDict["DEX"] = float(stats[j].replace(" DEX", ""))
+                # elif "SPD" in stats[j]:
+                #     statsDict["SPD"] = float(stats[j].replace(" SPD", ""))
+                # elif "WIS" in stats[j]:
+                #     statsDict["WIS"] = float(stats[j].replace(" WIS", ""))
+                # elif "VIT" in stats[j]:
+                #     statsDict["VIT"] = float(stats[j].replace(" VIT", ""))
+
+            weapon_dict["Stats"] = stats_dict
+    print(weapon_dict)
+    return weapon_dict
+
+
+def parse_weapons():
+    url_list = ["daggers", "bows", "staves", "wands", "swords", "katanas"]
     weapons_dict = {}
-    for i in URLarray :
-        weapons_dict.add({i: {}})
-    for j in URLarray:
+    for i in url_list:
+        weapons_dict[i] = {}
+    for j in url_list:
         req = Request('https://www.realmeye.com/wiki/' + j, headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(req).read()
         soup = bs.BeautifulSoup(webpage, "lxml")
         test = soup.findAll("a")
-        thisURL = "https://www.realmeye.com"
+        this_url = "https://www.realmeye.com"
         for i in test:
-            if (i.text == ""):
-                thisURL = thisURL + i.get("href")
-                temp = parseWeapon(thisURL)
-                if (temp["Tier"] == "UT" | temp["Tier"] == "ST"):
+            if i.text is "":
+                this_url = this_url + i.get("href")
+                temp = parse_weapon(this_url)
+                # noinspection PyTypeChecker
+                if temp["Tier"] is "UT" | temp["Tier"] is "ST":
                     weapons_dict[j][temp["Tier"]][temp["Name"]] = temp
                 else:
                     weapons_dict[j][temp["Tier"]]["Tiered"] = temp
+
     return weapons_dict
 
-def parseArmor(URL):
-    armorParseReq = Request(URL, headers = {"User-Agent": "Mozilla/5.0"})
-    armorPage = urlopen(armorParseReq).read()
-    armorSoup = bs.BeautifulSoup(armorPage, "lxml")
 
-    tempString = "https://www.realmeye.com/wiki/"
-    name = URL
-    name = name.replace(tempString, "")
+def parse_armor(url):
+    armor_parse_req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+    armor_page = urlopen(armor_parse_req).read()
+    armor_soup = bs.BeautifulSoup(armor_page, "lxml")
 
-    img = armorSoup.findAll("img")
-    imgURL = ""
+    temp_string = "https://www.realmeye.com/wiki/"
+    name = url
+    name = name.replace(temp_string, "")
+
+    img = armor_soup.findAll("img")
+    img_url = ""
     for j in range(len(img)):
-        if img[j].get("alt").replace(" ", "-").lower() == name:
-            imgURL = "https:" + img[j].get("src")
+        if img[j].get("alt").replace(" ", "-").lower() is name:
+            img_url = "https:" + img[j].get("src")
             break
-    armorDict = {"Name": name, "Img": imgURL, "Tier": -1, "Stats": -1}
-    armor = armorSoup.getText().split("\n")
+    armor_dict = {"Name": name, "Img": img_url, "Tier": -1, "Stats": -1}
+    armor = armor_soup.getText().split("\n")
     for i in range(len(armor)):
-        if armor[i] == "Tier":
+        if armor[i] is "Tier":
             if (armor[i + 1]).isdigit():
-                armorDict["Tier"] = int(armor[i + 1])
+                armor_dict["Tier"] = int(armor[i + 1])
             elif "UT" in armor[i + 1]:
-                armorDict["Tier"] = "UT"
+                armor_dict["Tier"] = "UT"
             elif "ST" in armor[i + 1]:
-                armorDict["Tier"] = "ST"
+                armor_dict["Tier"] = "ST"
             else:
                 raise ValueError
-        elif armor[i] == "On Equip":
-            statsDict = {"HP": 0, "MP": 0, "ATT": 0, "DEF": 0, "DEX": 0, "SPD": 0, "WIS": 0, "VIT": 0}
+        elif armor[i] is "On Equip":
+            stats_dict = {"HP": 0, "MP": 0, "ATT": 0, "DEF": 0, "DEX": 0, "SPD": 0, "WIS": 0, "VIT": 0}
             stats = armor[i+1].split(",")
             for j in range(len(stats)):
-                if "HP" in stats[j]:
-                    statsDict["HP"] = float(stats[j].replace(" HP", ""))
-                elif "MP" in stats[j]:
-                    statsDict["MP"] = float(stats[j].replace(" MP", ""))
-                elif "ATT" in stats[j]:
-                    statsDict["ATT"] = float(stats[j].replace(" ATT", ""))
-                elif "DEF" in stats[j]:
-                    statsDict["DEF"] = float(stats[j].replace(" DEF", ""))
-                elif "DEX" in stats[j]:
-                    statsDict["DEX"] = float(stats[j].replace(" DEX", ""))
-                elif "SPD" in stats[j]:
-                    statsDict["SPD"] = float(stats[j].replace(" SPD", ""))
-                elif "WIS" in stats[j]:
-                    statsDict["WIS"] = float(stats[j].replace(" WIS", ""))
-                elif "VIT" in stats[j]:
-                    statsDict["VIT"] = float(stats[j].replace(" VIT", ""))
-            armorDict["Stats"] = statsDict
-    print(armorDict)
-    return armorDict
+                arr = stats[j].split
+                stats_dict[arr[1]] = float(arr[0])
+                #
+                # if "HP" in stats[j]:
+                #     stats_dict["HP"] = float(stats[j].replace(" HP", ""))
+                # elif "MP" in stats[j]:
+                #     stats_dict["MP"] = float(stats[j].replace(" MP", ""))
+                # elif "ATT" in stats[j]:
+                #     stats_dict["ATT"] = float(stats[j].replace(" ATT", ""))
+                # elif "DEF" in stats[j]:
+                #     stats_dict["DEF"] = float(stats[j].replace(" DEF", ""))
+                # elif "DEX" in stats[j]:
+                #     stats_dict["DEX"] = float(stats[j].replace(" DEX", ""))
+                # elif "SPD" in stats[j]:
+                #     stats_dict["SPD"] = float(stats[j].replace(" SPD", ""))
+                # elif "WIS" in stats[j]:
+                #     stats_dict["WIS"] = float(stats[j].replace(" WIS", ""))
+                # elif "VIT" in stats[j]:
+                #     stats_dict["VIT"] = float(stats[j].replace(" VIT", ""))
+            armor_dict["Stats"] = stats_dict
+    print(armor_dict)
+    return armor_dict
 
-def parseArmors():
+
+def parse_armors():
     url_array = ["leather-armors", "robes", "heavy-armors"]
     armors_dict = {}
     for i in url_array:
-        armors_dict.add({i: {}})
-    for j in url_array:
-        req = Request("https://www.realmeye.com/wiki/" + j, headers = {"User-Agent": "Mozilla/5.0"})
-        webpage = urlopen(req).read()
-        soup = bs.BeautifulSoup(webpage, "lxml")
-        test = soup.findAll("a")
-        thisURL = "https://www.realmeye.com"
-        for i in test:
-            if (i.text == ""):
-                thisURL = thisURL + i.get("href")
-                temp = parseArmor(thisURL)
-                if (temp["Tier"] == "UT" or temp["Tier"] == "ST"):
-                    armors_dict[j][temp["Tier"]][temp["Name"]] = temp
-                else:
-                    armors_dict[j][temp["Tier"]]["Tiered"] = temp
-    return armors_dict
-
-def parseAbilities():
-    url_array = ["cloaks", "quivers", "spells", "tomes", "helms", "shields", "seals", "poisons", "skulls", "traps",
-                 "orbs", "prisms", "scepters", "stars", "wakizashi", "lutes"]
-    abilities_dict = {}
-    for i in url_array:
-        abilities_dict.add({i: {}})
+        armors_dict[i] = {}
     for j in url_array:
         req = Request("https://www.realmeye.com/wiki/" + j, headers={"User-Agent": "Mozilla/5.0"})
         webpage = urlopen(req).read()
         soup = bs.BeautifulSoup(webpage, "lxml")
         test = soup.findAll("a")
-        thisURL = "https://www.realmeye.com"
+        this_url = "https://www.realmeye.com"
         for i in test:
-            if (i.text == ""):
-                thisURL = thisURL + i.get("href")
-                temp = parseArmor(thisURL)
-                if (temp["Tier"] == "UT" or temp["Tier"] == "ST"):
+            if i.text is "":
+                this_url = this_url + i.get("href")
+                temp = parse_armor(this_url)
+                if temp["Tier"] is "UT" or temp["Tier"] is "ST":
+                    armors_dict[j][temp["Tier"]][temp["Name"]] = temp
+                else:
+                    armors_dict[j][temp["Tier"]]["Tiered"] = temp
+    return armors_dict
+
+
+def parse_abilities():
+    url_array = ["cloaks", "quivers", "spells", "tomes", "helms", "shields", "seals", "poisons", "skulls", "traps",
+                 "orbs", "prisms", "scepters", "stars", "wakizashi", "lutes"]
+    abilities_dict = {}
+    for i in url_array:
+        abilities_dict[i] = {}
+    for j in url_array:
+        req = Request("https://www.realmeye.com/wiki/" + j, headers={"User-Agent": "Mozilla/5.0"})
+        webpage = urlopen(req).read()
+        soup = bs.BeautifulSoup(webpage, "lxml")
+        test = soup.findAll("a")
+        this_url = "https://www.realmeye.com"
+        for i in test:
+            if i.text is "":
+                this_url = this_url + i.get("href")
+                temp = parse_armor(this_url)
+                if temp["Tier"] is "UT" or temp["Tier"] is "ST":
                     abilities_dict[j][temp["Tier"]][temp["Name"]] = temp
                 else:
                     abilities_dict[j][temp["Tier"]]["Tiered"] = temp
     return abilities_dict
 
-def parseRings():
-    url_array = [["health", "magic", "attack", "defense", "speed", "dexterity", "vitality", "wisdom"], "untiered", "limited"]
+
+def parse_rings():
+    url_array = [["health", "magic", "attack", "defense", "speed", "dexterity", "vitality", "wisdom"],
+                 "untiered", "limited"]
     rings_dict = []
     for i in range(len(url_array)):
-        if i == 0:
-            rings_dict.add({"tiered":{}})
+        if i is 0:
+            rings_dict.append({"tiered": {}})
         else:
-            rings_dict.add({url_array[i]:{}})
+            rings_dict.append({url_array[i]: {}})
     for j in range(len(url_array) + 8):
-        temp1 = ""
-        if (j < 8):
+        if j < 8:
             val = "tiered"
             temp1 = url_array[0][j]
-        elif j == 8:
+        elif j is 8:
             val = "untiered"
             temp1 = url_array[1]
         else:
@@ -216,28 +232,36 @@ def parseRings():
         webpage = urlopen(req).read()
         soup = bs.BeautifulSoup(webpage, "lxml")
         test = soup.findAll("a")
-        thisURL = "https://www.realmeye.com"
+        this_url = "https://www.realmeye.com"
         for i in test:
-            if (i.text == ""):
-                thisURL = thisURL + i.get("href")
-                temp = parseArmor(thisURL)
-                if (temp["Tier"] == "UT" or temp["Tier"] == "ST"):
+            if i.text is "":
+                this_url = this_url + i.get("href")
+                temp = parse_armor(this_url)
+                if temp["Tier"] is "UT" or temp["Tier"] is "ST":
+                    # noinspection PyTypeChecker
                     rings_dict[val][temp["Tier"]][temp["Name"]] = temp
                 else:
+                    # noinspection PyTypeChecker
                     rings_dict[val][temp["Tier"]]["Tiered"] = temp
     return rings_dict
 
-def calculateDPS(weapon, ring, armor, ability, stats):
+
+def calculate_dps(weapon, ring, armor, ability, stats):
     player_stats = stats
     for i in stats:
         player_stats[i] = player_stats[i] + ring[i] + ability[i] + armor[i] + weapon["Stats"][i]
 
-def parseAll():
+
+def parse_all():
     all_equipables = {}
     types = ["Weapons", "Armors", "Abilities", "Rings"]
     for i in types:
-        all_equipables.add({i:{}})
-    all_equipables["Weapons"] = parseWeapons()
-    all_equipables["Armors"] = parseArmors()
-    all_equipables["Abilities"] = parseAbilities()
-    all_equipables["Rings"] = parseRings()
+        all_equipables[i] = {}
+
+    all_equipables["Weapons"] = parse_weapons()
+    all_equipables["Armors"] = parse_armors()
+    all_equipables["Abilities"] = parse_abilities()
+    all_equipables["Rings"] = parse_rings()
+
+
+parse_weapon("https://www.realmeye.com/wiki/steel-dagger")
